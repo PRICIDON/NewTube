7a49dbd13a76c0b7300e4efa6ddcaf24fc3693fd
dddbj <dddbjjk6@gmail.com>
2025-03-25 16:27:59 +0300

    First Commit

---
+++ components/home-sidebar/section.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { type LucideIcon } from "lucide-react";
+import {
+    SidebarGroup,
+    SidebarGroupContent, SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+
+interface Item {
+    title: string;
+    url: string;
+    icon: LucideIcon;
+    auth?: boolean;
+}
+
+interface Props {
+    items: Item[]
+    label?: string
+}
+
+export default function Section({ items, label }: Props) {
+    return (
+        <SidebarGroup>
+            {label && <SidebarGroupLabel>{label}</SidebarGroupLabel>}
+            <SidebarGroupContent>
+                <SidebarMenu>
+                    {items.map((item) => (
+                        <SidebarMenuItem key={item.title}>
+                            <SidebarMenuButton tooltip={item.title} asChild isActive={false} onClick={() => {}}>
+                                <Link href={item.url} className="flex items-center gap-4">
+                                    <item.icon />
+                                    <span className="text-sm">{item.title}</span>
+                                </Link>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    ))}
+                </SidebarMenu>
+            </SidebarGroupContent>
+        </SidebarGroup>
+    )
+}
