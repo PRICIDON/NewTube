b91ce82c2435144af02b3f1476c2c93cfeea64a0
dddbj <dddbjjk6@gmail.com>
2025-04-03 21:09:48 +0300

    Тест вебхука MUX 2

--- app/(studio)/studio/page.tsx
+++ app/(studio)/studio/page.tsx
@@ -4,7 +4,7 @@ import StudioView from "@/components/views/studio-view";
 import {DEFAULT_LIMIT} from "@/lib/constants";
 
 export default async function Page() {
-    void trpc.studio.getMany.prefetchInfinite({
+    await trpc.studio.getMany.prefetchInfinite({
         limit: DEFAULT_LIMIT
     });
 
