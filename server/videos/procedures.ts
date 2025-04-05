dc538a12b6781a980fde56c8db9d0d3ae1c6fb3e
dddbj <dddbjjk6@gmail.com>
2025-04-05 20:39:39 +0300

    UPSTASH Workflow test

--- server/videos/procedures.ts
+++ server/videos/procedures.ts
@@ -116,11 +116,11 @@ export const videosRouter = createTRPCRouter({
         }).where(and(eq(videos.id, input.id),eq(videos.userId, userId))).returning();
         return { updatedVideo }
     }),
-    generateThumbnail: protectedProcedure.input(z.object({id: z.string().uuid()})).mutation(async ({ ctx }) => {
+    generateThumbnail: protectedProcedure.input(z.object({id: z.string().uuid()})).mutation(async ({ ctx, input }) => {
         const { id: userId } = ctx.user
         const { workflowRunId } = await workflow.trigger({
             url: `${process.env.UPSTASH_WORKFLOW_URL}/api/videos/workflows/title`,
-            body: { userId },
+            body: { userId, videoId: input.id  },
         })
 
         return workflowRunId
