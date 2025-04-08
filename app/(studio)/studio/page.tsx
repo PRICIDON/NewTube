edc0aaf118e31c49c6225034ab891d525e896bc5
dddbj <dddbjjk6@gmail.com>
2025-04-08 19:56:57 +0300

    Video comments test

--- app/(studio)/studio/page.tsx
+++ app/(studio)/studio/page.tsx
@@ -3,6 +3,8 @@ import VideosSection from "@/components/studio/videos-section";
 import {HydrateClient, trpc} from "@/trpc/server";
 import {DEFAULT_LIMIT} from "@/lib/constants";
 
+export const dynamic = "force-dynamic"
+
 export default function Page() {
     void trpc.studio.getMany.prefetchInfinite(
         {  limit: DEFAULT_LIMIT },
