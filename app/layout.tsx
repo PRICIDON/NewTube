ee687e834236ef12a3d00735454cb1fccbdeb0de
dddbj <dddbjjk6@gmail.com>
2025-04-10 22:36:23 +0300

    Theme test

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default async function RootLayout({
             >
               <TRPCProvider>
                   <NextIntlClientProvider messages={messages}>
-                    <ThemeProvider attribute="class" defaultTheme="light">
+                    <ThemeProvider attribute="class" defaultTheme="dark">
                       <Toaster />
                       {children}
                     </ThemeProvider>
